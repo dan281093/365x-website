a8a1f5f32037a7d6787e6ec52a43e8b9a5e04ae8
dan amouyal <danamouyal@hotmail.fr>
2019-09-19 21:12:15 +0300

    some fix

--- public/scripts.js
+++ public/scripts.js
@@ -152,28 +152,33 @@ $(document).ready(function(){
   });
 });
 
-$('#carousel-team, #carousel-coaches, #carousel-mentor').on('slide.bs.carousel', function (e) {
-  var $e = $(e.relatedTarget);
-  var idx = $e.index();
-  var itemsPerSlide = 4;
-  var totalItems = $('.carousel-item').length;
-
-  if (idx >= totalItems-(itemsPerSlide-1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i=0; i<it; i++) {
-          // append slides to end
-          if (e.direction === "left") {
-              $('.carousel-item').eq(i).appendTo('.carousel-inner');
-          }
-          else {
-              $('.carousel-item').eq(0).appendTo('.carousel-inner');
-          }
-      }
-    }
-  });
-});
-
 
 /*
     Carousel
 */
+$('#carousel-mentor, #carousel-coaches').on('slide.bs.carousel', function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+      console.log('lol')
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction === "left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+
+
+
+
+});
